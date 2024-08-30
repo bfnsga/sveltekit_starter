d4a0b37caf06fca7af8aa07f6e9653491135d6d3
Ben Field <bfnsga@gmail.com>
2024-08-30 09:36:15 +0200

    build(pnpm): removed unused dependencies

--- src/lib/clients.ts
+++ src/lib/clients.ts
@@ -1,11 +1,9 @@
 import { Resend } from 'resend';
-import { Unkey } from '@unkey/api';
 import { createClient } from '@supabase/supabase-js';
 import {
 	RESEND_API_KEY,
 	STRIPE_API_KEY,
-	SUPABASE_SERVICE_ROLE_KEY,
-	UNKEY_ROOT_KEY
+	SUPABASE_SERVICE_ROLE_KEY
 } from '$env/static/private';
 import { PUBLIC_SUPABASE_URL } from '$env/static/public';
 import Stripe from 'stripe';
@@ -15,5 +13,3 @@ export const resend = new Resend(RESEND_API_KEY);
 export const stripe = new Stripe(STRIPE_API_KEY);
 
 export const supabaseAuth = createClient(PUBLIC_SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
-
-export const unkey = new Unkey({ rootKey: UNKEY_ROOT_KEY });
